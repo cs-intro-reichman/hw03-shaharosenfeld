80082252c1d9b40176d84440e340bfc9710ba21c
shahar rosenfeld <shahar.rosenfeld@runi.ac.il>
2024-01-03 16:33:08 +0200

    (:

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,63 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String newWord= "" + s.charAt(0);
+        for(int i=0;i< s.length()-1; i++)
+        {
+            for(int j=0; j<newWord.length();j++)
+            {
+                
+                if (newWord.charAt(j)==s.charAt(i)) {
+                j=newWord.length()+10;
+                }
+                if(j==newWord.length()-1)
+                {   
+                    newWord=newWord+s.charAt(i);
+                }
+                if(s.charAt(i)==' ')
+                {
+                    newWord=newWord+s.charAt(i);
+                    j=newWord.length()+10;
+                }
+            }
+        }
+       
+       
+       
+       
+       return newWord;
+       
+        /*for(int i =1; i<=s.length()-1; i++)
+        {
+            int j =0;
+            if(i!=s.length()-1)
+            {
+                 j = i+1;
+            }
+            else{
+                 j=0;
+                 newWord= newWord+s.charAt(i);
+            }
+            if(s.charAt(i)==' ')
+            {
+                 j=0;
+                newWord= newWord+s.charAt(i);
+            }
+            while(j!= 0)
+            {
+             if(s.charAt(i) == s.charAt(j))
+                {
+                    j=-1;
+                } 
+             if(j == s.length()-1)
+                {
+                    newWord= newWord+s.charAt(i);
+                    j=-1;
+                }
+                j++;
+            }   
+        }
+       //  newWord = newWord + s.charAt(s.length()-1);
+        return newWord; */
     }
 }
